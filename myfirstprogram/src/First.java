0fb7cf3100174d0c78b67132bd8b41a41a237bc4
Yu.Lu23 <yuelyulu5206@gmail.com>
2024-07-24 09:26:37 +0800

    Update First.java
    2 days

--- myfirstprogram/src/First.java
+++ myfirstprogram/src/First.java
@@ -1,8 +1,43 @@
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class First {
 
     public static void main(String[] args) {
+
+
+
+        
+       // package of jumping "chat frame"
+        String name = JOptionPane.showInputDialog("Enter your name");
+        JOptionPane.showMessageDialog(null, "Hello "+name);
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        JOptionPane.showMessageDialog(null,"You are "+age+"years old");
+
+        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
+        JOptionPane.showMessageDialog(null,"You are "+height+"cm tall");
+
+
+
+
+
+
+
+
+
+
+
+        //expression = operands & operators
+        // operands= values, variables, numbers,quantity
+        //operators = +_*/%
+
+        //int friends = 10;
+
+        //friends = friends % 3; //reminder
+
+        //System.out.println(friends);
+
 
         Scanner scanner = new Scanner(System.in);
 
